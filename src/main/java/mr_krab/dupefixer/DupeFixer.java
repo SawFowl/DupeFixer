fe10cb4d332850751643bbb53edd0853a863b5c2
Иван <StalkerIv@yandex.ru>
2019-06-13 09:48:20 +0400

    Attempt to fix dupe with portals.

--- src/main/java/mr_krab/dupefixer/DupeFixer.java
+++ src/main/java/mr_krab/dupefixer/DupeFixer.java
@@ -17,6 +17,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,6 +40,7 @@ import me.ryanhamshire.griefprevention.GriefPrevention;
 import mr_krab.dupefixer.listeners.DropListener;
 import mr_krab.dupefixer.listeners.InteractItemListenerFluidFixGP;
 import mr_krab.dupefixer.listeners.InteractItemListenerFluidFixRP;
+import mr_krab.dupefixer.listeners.PortalListener;
 import mr_krab.dupefixer.listeners.ShiftClickListener;
 import mr_krab.dupefixer.utils.ConfigUtil;
 import mr_krab.dupefixer.utils.ProtectPluginsAPI;
@@ -46,7 +49,7 @@ import ninja.leaping.configurate.yaml.YAMLConfigurationLoader;
 
 @Plugin(id = "dupefixer",
 	name = "DupeFixer",
-	version = "1.2",
+	version = "1.3",
 	authors = "Mr_Krab",
 	dependencies = {
 		@Dependency(id = "griefprevention", optional = true),
@@ -71,8 +74,14 @@ public class DupeFixer {
 	private static DupeFixer instance;
 	private static ConfigUtil configUtil;
 	private static ProtectPluginsAPI protectPluginsAPI;
+	private static DropListener dropListener;
+	private static InteractItemListenerFluidFixGP iGp;
+	private static InteractItemListenerFluidFixRP iRp;
+	private static PortalListener portalListener;
+	private static ShiftClickListener shiftClickListener;
 	
 	boolean foundRP = false;
+	List<String> enableListeners = new ArrayList<String>();
 
 	public Path getConfigDir() {
 		return configDir;
@@ -93,15 +102,6 @@ public class DupeFixer {
 		return protectPluginsAPI;
 	}
 
-	public void load() {
-		configLoader = YAMLConfigurationLoader.builder().setPath(configDir.resolve("config.yml")).setFlowStyle(FlowStyle.BLOCK).build();
-		try {
-			rootNode = configLoader.load();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
 	@Listener
 	public void onPostInitialization(GamePostInitializationEvent event) {
 		logger = (Logger)LoggerFactory.getLogger("DupeFixer");
@@ -110,7 +110,7 @@ public class DupeFixer {
 		load();
 		configUtil.checkConfigVersion();
 		protectPluginsAPI = new ProtectPluginsAPI();
-		registerListeners();
+		updateListeners();
 	}
 
 	@Listener
@@ -124,31 +124,85 @@ public class DupeFixer {
 	@Listener
 	public void onReload(GameReloadEvent event) {
 		load();
+		updateListeners();
+	}
+
+	public void load() {
+		configLoader = YAMLConfigurationLoader.builder().setPath(configDir.resolve("config.yml")).setFlowStyle(FlowStyle.BLOCK).build();
+		try {
+			rootNode = configLoader.load();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	private void registerListeners() {
-		if(rootNode.getNode("FixContainer", "Enable").getBoolean()) {
-			Sponge.getEventManager().registerListeners(this, new DropListener(this));
+	private void updateListeners() {
+		if(rootNode.getNode("FixContainer", "Enable").getBoolean() && !enableListeners.contains("DropListener")) {
+			dropListener = new DropListener(this);
+			Sponge.getEventManager().registerListeners(this, dropListener);
+			enableListeners.add("DropListener");
+		} else {
+			if(dropListener != null) {
+				Sponge.getEventManager().unregisterListeners(dropListener);
+				enableListeners.remove("DropListener");
+				dropListener = null;
+			}
 		}
+		
 		if(rootNode.getNode("FixFluidDupe", "Enable").getBoolean()) {
-			if (Sponge.getPluginManager().isLoaded("griefprevention")) {
+			iRp = new InteractItemListenerFluidFixRP(this);
+			if (Sponge.getPluginManager().isLoaded("griefprevention") && !enableListeners.contains("InteractItemListenerFluidFixGP")) {
 				protectPluginsAPI.setGriefPreventionAPI(GriefPrevention.getApi());
-				Sponge.getEventManager().registerListeners(this,new InteractItemListenerFluidFixGP(this));
+				iGp = new InteractItemListenerFluidFixGP(this);
+				Sponge.getEventManager().registerListeners(this, iGp);
+				enableListeners.add("InteractItemListenerFluidFixGP");
+			} else {
+				if(iGp != null) {
+					Sponge.getEventManager().unregisterListeners(iGp);
+					enableListeners.remove("InteractItemListenerFluidFixGP");
+					iGp = null;
+				}
 			}
 			/*
 			 * Руки оторвать бы разработчику RedProtect за то, как он предоставляет API своего плагина.
 			 * The RedProtect developer needs to tear off his hands for how he provides his plugin API.
 			 */
-			if(Sponge.getPluginManager().getPlugin("redprotect").isPresent()) {
+			if(Sponge.getPluginManager().getPlugin("redprotect").isPresent() && !enableListeners.contains("InteractItemListenerFluidFixRP")) {
 				foundRP = true;
-				Sponge.getEventManager().registerListeners(this,new InteractItemListenerFluidFixRP(this));
+				Sponge.getEventManager().registerListeners(this, iRp);
+				enableListeners.add("InteractItemListenerFluidFixRP");
+			} else {
+				if(iRp != null) {
+					Sponge.getEventManager().unregisterListeners(iRp);
+					enableListeners.remove("InteractItemListenerFluidFixRP");
+					iRp = null;
+				}
 			}
 			if(!protectPluginsAPI.isPresentGP() && !foundRP) {
 				logger.error("None of the supported claims protection plugins were found!");
 			}
 		}
-		if(rootNode.getNode("BlockShiftClick", "Enable").getBoolean()) {
-			Sponge.getEventManager().registerListeners(this, new ShiftClickListener(this));
+		if(rootNode.getNode("BlockShiftClick", "Enable").getBoolean() && !enableListeners.contains("ShiftClickListener")) {
+			shiftClickListener = new ShiftClickListener(this);
+			Sponge.getEventManager().registerListeners(this, shiftClickListener);
+			enableListeners.add("ShiftClickListener");
+		} else {
+			if(shiftClickListener != null) {
+				Sponge.getEventManager().unregisterListeners(shiftClickListener);
+				enableListeners.remove("ShiftClickListener");
+				shiftClickListener = null;
+			}
+		}
+		if(rootNode.getNode("PortalLock", "Enable").getBoolean() && !enableListeners.contains("PortalListener")) {
+			portalListener = new PortalListener(this);
+			Sponge.getEventManager().registerListeners(this, portalListener);
+			enableListeners.add("PortalListener");
+		} else {
+			if(portalListener != null) {
+				Sponge.getEventManager().unregisterListeners(portalListener);
+				enableListeners.remove("PortalListener");
+				portalListener = null;
+			}
 		}
 	}
 }
